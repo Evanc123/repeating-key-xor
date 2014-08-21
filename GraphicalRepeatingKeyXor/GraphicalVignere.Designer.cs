8484e0cbcdbfe57b8713887810d3e9c8be868138
Evanc123 <evan.ecater@gmail.com>
2014-08-20 22:11:11 -0400

    First Commit

---
+++ GraphicalRepeatingKeyXor/GraphicalVignere.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace GraphicalRepeatingKeyXor
+{
+    partial class GraphicalVignere
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.submitButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(30, 139);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 0;
+            this.submitButton.Text = "Encrypt";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.Encrypt);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(30, 34);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(214, 20);
+            this.inputTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(188, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Text to be Encrypted/Decrypted";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Key Here";
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(30, 96);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.Size = new System.Drawing.Size(214, 20);
+            this.keyTextBox.TabIndex = 4;
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Location = new System.Drawing.Point(30, 203);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.Size = new System.Drawing.Size(214, 20);
+            this.outputTextBox.TabIndex = 5;
+            this.outputTextBox.Visible = false;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(27, 178);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(85, 13);
+            this.outputLabel.TabIndex = 6;
+            this.outputLabel.Text = "Encrypted Text: ";
+            this.outputLabel.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Evan Cater - 2014";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(169, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Decrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Decrypt);
+            // 
+            // GraphicalVignere
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 260);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.keyTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.submitButton);
+            this.Name = "GraphicalVignere";
+            this.ShowIcon = false;
+            this.Text = "Repeating Key Xor Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GraphicalRepeatingKeyXor/GraphicalVignere.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicalRepeatingKeyXor
+{
+    public partial class GraphicalVignere : Form
+    {
+        public GraphicalVignere()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputTextBox.Text) || string.IsNullOrEmpty(keyTextBox.Text)) return;
+            var key = StringToByteArray(keyTextBox.Text);
+            var input = StringToByteArray(inputTextBox.Text);
+            var encryptedBytes = BitConverter.ToString(RepeatingKeyXorFunc(input, key));
+            outputTextBox.Text = encryptedBytes;
+            outputTextBox.Visible = true;
+            outputLabel.Visible = true;
+        }
+        private static byte[] RepeatingKeyXorFunc(IList<byte> bytesToEncrypt, IList<byte> key)
+        {
+            var encryptedByteArray = new byte[bytesToEncrypt.Count];
+            var keyCount = 0;
+            for (var i = 0; i < bytesToEncrypt.Count; i++)
+            {
+                encryptedByteArray[i] = (keyCount == key.Count)
+                    ? Xor(bytesToEncrypt[i], key[keyCount = 0])
+                    : Xor(bytesToEncrypt[i], key[keyCount]);
+
+                keyCount++;
+            }
+            return encryptedByteArray;
+        }
+
+        private static byte[] StringToByteArray(string inString)
+        {
+            char[] characters = inString.ToCharArray();
+            var byteArray = new byte[characters.Length];
+            var index = 0;
+            foreach (var character in characters)
+            {
+                byteArray[index] = (byte)character;
+                index++;
+            }
+            return byteArray;
+        }
+        static byte[] ConvertFromHexToByteArray(string hex)
+        {
+            var inputArray = new byte[hex.Length / 2];
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                inputArray[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return inputArray;
+        }
+        private static byte Xor(byte b, int c)
+        {
+            return (byte)(b ^ c);
+        }
+
+        private void Decrypt(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inputTextBox.Text) || string.IsNullOrEmpty(keyTextBox.Text)) return;
+            var key = StringToByteArray(keyTextBox.Text);
+            var input = ConvertFromHexToByteArray(inputTextBox.Text.Replace("-", string.Empty));
+            outputTextBox.Text = String.Empty;
+            var outputText = System.Text.Encoding.ASCII.GetString(RepeatingKeyXorFunc(input, key));
+            outputTextBox.Text = outputText;
+            outputTextBox.Visible = true;
+            outputLabel.Visible = true;
+        }
+    }
+}
+
+
